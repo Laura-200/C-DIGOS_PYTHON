e5de0aedf34196b32db2eb73c516c791f898b4ba
Laura-200 <laura.maga200@gmail.com>
2026-05-16 08:51:45 -0300

    adicionando código que testa API

--- login.py
+++ login.py
@@ -4,6 +4,6 @@ print("-" * 50)
 print("\n")
 
 if nomeUsuario == "Laura" and senhaUsuario == "1234":
-    print("Login bem-sucedido! Bem-vindo, Laura!🎉") 
+    print("Login bem-sucedido! Bem-vindo, Laura!🎉")
 else:
     print("Login falhou! Verifique seu nome de usuário e senha.😞")
